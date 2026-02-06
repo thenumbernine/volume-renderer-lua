4d81b2d4615a20792cf079683dd00cf8cd353963
Christopher Moore <christopher.e.moore@gmail.com>
2026-02-06 20:36:06 +0700

    more

---
+++ README.md
@@ -0,0 +1,14 @@
+TODO make an invokable class with this, similar to force-directed-graph, plot2d, plot3d, etc
+
+esp so I can put my raytracer / slicer in one place
+
+esp since now I've got earthquake-shear-lines/viz-hist-splat-volume.lua doing it
+
+test demo here: mandelbrot set
+
+another good one: the hydrogen wavefunction visualizer i've already got in webgl
+
+another good one / thing to integrate: hydro-cl
+
+
+Man I did this so much better 20 years ago, raytracing and everything.
--- run.lua
+++ run.lua
@@ -1,15 +1,4 @@
 #!/usr/bin/env luajit
---[[
-TODO make an invokable class with this, similar to force-directed-graph, plot2d, plot3d, etc
-esp so I can put my raytracer / slicer in one place
-esp since now I've got earthquake-shear-lines/viz-hist-splat-volume.lua doing it
-
-test demo here: mandelbrot set
-
-another good one: the hydrogen wavefunction visualizer i've already got in webgl
-
-another good one / thing to integrate: hydro-cl
---]]
 local ffi = require 'ffi'
 local table = require 'ext.table'
 local range = require 'ext.range'
